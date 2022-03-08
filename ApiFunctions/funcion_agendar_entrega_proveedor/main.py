178902dfbe074435fa69c6663d6b1f44a7fbdf37
vquirozuni <v.quiroz@uniandes.edu.co>
2022-03-07 20:33:16 -0500

    punto 4 completo

--- ApiFunctions/funcion_agendar_entrega_proveedor/main.py
+++ ApiFunctions/funcion_agendar_entrega_proveedor/main.py
@@ -1,16 +1,18 @@
 from logging import exception
 from google.cloud import tasks_v2
+from google.cloud import datastore
+from flask import jsonify
 import requests
 import os
 import json
 
-location_id = os.environ.get('LOCATION_ID', '')
-projec_id = os.environ.get('PROJECT_ID', '')
-queue_id = os.environ.get('QUEUE_ID', '')
-url_function_consumidor = os.environ.get('URL_FUNCTION_CONSUMIDOR', '')
+
 url_api_envio_agendar = os.environ.get('URL_API_ENVIO_AGENDAR', '')
 
 client = tasks_v2.CloudTasksClient()
+datastore_client = datastore.Client()
+kind = "Agenda"
+
 
 def funcion_agendar_entrega_proveedor(event):
     data = event['data']
@@ -20,34 +22,27 @@ def funcion_agendar_entrega_proveedor(event):
     try:
         envio_agendar_response = requests.post(url_api_envio_agendar, json = {"direccion-origen": "direccion-origen","direccion-destino": "direccion-destino"})  
         if envio_agendar_response.status_code != 200:     
-            return "Pedido No actualizado", envio_agendar_response.status_code
+            return "No se agendó", envio_agendar_response.status_code
 
-        print("Inicia Productor")
-        parent = client.queue_path(projec_id, location_id, queue_id)
-        task = {
-            "http_request": {  
-                "http_method": tasks_v2.HttpMethod.POST,
-                "url": url_function_consumidor,
-                "headers": {
-                    "Content-type": "application/json"
-                },
-                'body':json.dumps({'id':envio_agendar_response.json()['id'],'sellerId':data['sellerId'], 'userId': data['userId'], 'orderId':data['orderId'], 'estado':data['estado'], 'pickupDate':data['pickupDate'], 'deliveryDate':data['deliveryDate']}).encode()
-            }
-        }
-        response = client.create_task(parent= parent, task= task)
+        print("Inicia Copia a BD")
+        task_key = datastore_client.key(kind)
+        nueva_agenda = datastore.Entity(key=task_key)
+        nueva_agenda["id"] = envio_agendar_response.json()['id']
+        nueva_agenda["sellerId"] = data['sellerId']
+        nueva_agenda["userId"] = data['userId']
+        nueva_agenda["orderId"] = data['orderId']
+        nueva_agenda["estado"] = data['estado']
+        nueva_agenda["pickupDate"] = data['pickupDate']
+        nueva_agenda["deliveryDate"] = data['deliveryDate']
 
-        return {
-            'message': 'Se crea la tarea Cola de entregas de manera exitosa',
-            'name': response.name,
-            'http_request': {
-                'url' : response.http_request.url,
-                'http_method' : str(response.http_request.http_method)
-            }
-        }    
+        datastore_client.put(nueva_agenda)
+
+
+        return jsonify(nueva_agenda)
         
     except Exception as e:
-        print("No se agendó la entrega")
+        print("Error al registrar agenda")
         print(e)
-        return e, 404
+        return e, 400
 
-# gcloud functions deploy funcion-entrega3-agendar-entrega-proveedor --entry-point funcion_agendar_entrega_proveedor --runtime python39 --trigger-http --allow-unauthenticated --memory 128MB --region us-central1 --timeout 60 --min-instances 0 --max-instances 1 --service-account "misw-entrega3-atormentados@entrega3-343303.iam.gserviceaccount.com" --set-env-vars QUEUE_ID=cola-entregas,LOCATION_ID=us-central1,PROJECT_ID=entrega3-343303,URL_FUNCTION_CONSUMIDOR=https://us-central1-entrega3-343303.cloudfunctions.net/funcion-entrega3-crear-agenda,URL_API_DISPOSITIVOS=https://gwintegraciones-baqkkpep.uc.gateway.dev/v1/envio/agendar
+# gcloud functions deploy funcion-entrega3-agendar-entrega-proveedor --entry-point funcion_agendar_entrega_proveedor --runtime python39 --trigger-http --allow-unauthenticated --memory 128MB --region us-central1 --timeout 60 --min-instances 0 --max-instances 1 --service-account "misw-entrega3-atormentados@entrega3-343303.iam.gserviceaccount.com" --set-env-vars URL_API_DISPOSITIVOS=https://gwintegraciones-baqkkpep.uc.gateway.dev/v1/envio/agendar
